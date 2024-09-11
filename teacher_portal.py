bb231b690c1373641da25fe8d7322acf41c9f3f9
Mark Bodenchak <75702325+mbodenchak@users.noreply.github.com>
2024-09-11 14:09:56 -0700

    add placeholder menu options

--- teacher_portal.py
+++ teacher_portal.py
@@ -35,7 +35,14 @@ def mainMenu():
       print("\nMain Menu:")
       print("1. Add Student")
       print("2. List All Students")
-      print("3. Exit")
+      print("3. View Students Assignments")
+      print("4. Add Grade")
+      print("5. Edit Grade")
+      print("6. Delete Grade")
+      print("7. Delete Student")
+      print("8. Edit Gradebook Weights")
+      print("9. Calculate Overall Grade")
+      print("10. Exit")
       
       choice = input("Choose an option: ")
       if choice == '1':
@@ -43,6 +50,20 @@ def mainMenu():
       elif choice == '2':
         listStudents()
       elif choice == '3':
+        print("View Assignments")
+      elif choice == '4':
+        print("Add Grade")
+      elif choice == '5':
+        print("Edit Grade")
+      elif choice == '6':
+        print("Delete Grade")
+      elif choice == '7':
+        print("Delete Student")
+      elif choice == '8':
+        print("Edit Gradebook Weights")
+      elif choice == '9':
+        print("Calculate Overall Grade")
+      elif choice == '10':
         print("Exiting program...")
         break
       else:
